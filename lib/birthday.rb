8b6f068afe1060c197f1f64942831588fbcc6ef0
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-02 03:01:43 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,16 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each {|name_age|
+name = name_age[0]
+age = name_age[1]
+puts "Happy Birthday #{name}! You are now #{age} years old!"
+}
+
+
 end
-
-
